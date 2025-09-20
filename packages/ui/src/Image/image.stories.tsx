fb8d582ce431f704a9c68f6254f8269aef88272a
moonjjj <mjesun1@naver.com>
2025-09-20 11:00:56 +0900

    base app

--- packages/ui/src/Image/image.stories.tsx
+++ packages/ui/src/Image/image.stories.tsx
@@ -1,4 +1,5 @@
-import {Image, RATIO_MAP, BaseImageProps} from '.'
+import type {BaseImageProps} from '.'
+import {Image, RATIO_MAP} from '.'
 
 const meta = {
     title: 'base/image',
@@ -9,18 +10,30 @@ const meta = {
                 type: 'select',
                 options: Object.keys(RATIO_MAP),
             },
-            description: '이미지 비율',
+            description: '이미지 비율 (aspect-ratio)',
+        },
+        width: {
+            control: 'number',
+            description: '컨테이너의 width (예: "120px", "50%")',
+        },
+        height: {
+            control: 'number',
+            description: '컨테이너의 height (예: "180px", "auto")',
+        },
+        radius: {
+            control: 'number',
+            description: '컨테이너의 borderRadius (예: "8px", "50%")',
         },
     },
 }
 
 export default meta
 
-export const 이미지 = ({size}: BaseImageProps) => {
-    return <Image src="https://picsum.photos/200/300" size={size} />
+export const 이미지 = (props: BaseImageProps) => {
+    return <Image {...props} />
 }
 
-
-
-
-
+이미지.args = {
+    src: 'https://picsum.photos/1000/1000',
+    radius: 8,
+}
--- packages/ui/src/Image/index.tsx
+++ packages/ui/src/Image/index.tsx
@@ -1,24 +1,35 @@
 import classNames from 'classnames/bind'
-import styles from './image.module.scss'
+import styles from './Image.module.scss'
 
 const cx = classNames.bind(styles)
 
 export const RATIO_MAP = {
-    '4:1': 'mini',
-    '5:3': 'small',
-    '4:3': 'medium',
-    '1:1': 'large',
-    '10:11': 'big',
+    '4:1': 'aspect-4-1',
+    '5:3': 'aspect-5-3',
+    '4:3': 'aspect-4-3',
+    '1:1': 'aspect-square',
+    '10:11': 'aspect-10-11',
 }
 
 export interface BaseImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
     src: string
     size?: keyof typeof RATIO_MAP
+    width?: number
+    height?: number
+    radius?: number
 }
 
-export function Image({src, size = '1:1', ...props}: BaseImageProps) {
+export function Image({src, size = '1:1', width, height, style, radius, className, ...props}: BaseImageProps) {
     return (
-        <div className={cx('img-container', RATIO_MAP[size])}>
+        <div
+            className={cx('container', RATIO_MAP[size], className)}
+            style={{
+                width: width ?? '100%',
+                height: height ?? 'auto',
+                borderRadius: radius,
+                ...style,
+            }}
+        >
             <img src={src} loading="lazy" {...props} />
         </div>
     )
